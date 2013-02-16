ce476b729b424212eccd7b15dc1f2320359ae7cf
mattypreston <Matthew.Preston@Torex.com>
2013-02-16 13:41:36 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MediaJfx::Application.config.secret_token = '92dadee64e848b2bddca8d501580f4313bed54b8db42a546737d787dae4a7e55b8c10f5fccae3594eb316109a9a6b15cc2188a97ea13c2ba5dff3cc7e114e26b'
