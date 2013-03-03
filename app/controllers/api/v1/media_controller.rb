78aec28148a580eba5208132c92345f722536bcb
mattypreston <Matthew.Preston@Torex.com>
2013-03-03 18:38:29 +0000

    Added media_controller for api.

---
+++ app/controllers/api/v1/media_controller.rb
@@ -0,0 +1,30 @@
+module Api
+  module V1
+    class MediaController < ApplicationController
+      respond_to :xml, :json
+
+      def get_fxml
+        @errors = []
+        begin
+          if params[:device_id].present?
+            @schedules = Schedule.where("device_id = ?", params[:device_id])
+            if @schedules.empty?
+              @errors << {:error_no => 1, :message => "no packages found for device_id : #{params[:device_id]}"}
+            end
+          else
+            @errors << {:error_no => 2, :message => "no device id provided"}
+          end
+        rescue Exception => e
+          @errors << {:error_no => 99, :message => e.message}
+        end
+        respond_to do |format|
+          if @errors.empty?
+            format.json {render :json => @schedules, :include => :package}
+          else
+            format.json {render :json => {:errors => @errors}}
+          end
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,82 +2,19 @@ require 'api_constraints'
 
 MediaJfx::Application.routes.draw do
   resources :schedules
-
-
   resources :packages
-
-
   resources :devices
-
-
   resources :sites
-
-
   get "home/index"
-
   devise_for :users
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
 
   namespace :api, defaults: {format: 'json'} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       get 'users/get_users/:device_id' => 'users#get_users'
+      get 'media/get_fxml/:device_id' => 'media#get_fxml'
     end
   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
   root :to => 'home#index'
 
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
