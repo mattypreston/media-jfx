613c48ab92086793d0fd63cc04f4ae5c8f0a4630
mattypreston <Matthew.Preston@Torex.com>
2013-03-21 20:20:48 +0000

    Now returning fxml in get_package api request.

--- app/controllers/api/v1/ping_controller.rb
+++ app/controllers/api/v1/ping_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class ErrorController < ApplicationController
+    class PingController < ApplicationController
       respond_to :xml, :json
 
     end
--- app/models/package.rb
+++ app/models/package.rb
@@ -5,16 +5,12 @@ class Package < ActiveRecord::Base
   include Rails.application.routes.url_helpers
 
   def as_json(options)
-    #hash = super(options.merge!({:include => :assets}))
-    hash = super(options.merge({:only => :id}).merge({:include => :assets}))
+    hash = super(options.merge({:only => [:id, :name, :fxml]}).merge({:include => :assets}))
     hash[:assets].each do |asset|
       asset_url = "#{MEDIA_JFX_CONFIG['url_prefix']}#{asset['asset_file']['url']}" if asset['asset_file']['url'].present?
-      puts asset_url
       asset['asset_file']['url'] = asset_url if asset_url.present?
     end
-
-
-
+    hash
   end
 
 end
