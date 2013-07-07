dec6cd90d9fbf91ea4fb518f22ba64e951ec9bd8
mattypreston <Matthew.Preston@Torex.com>
2013-07-07 13:08:29 +0100

    Issue with assets precompiling.

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,8 @@ module MediaJfx
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.assets.precompile += ['fullcalendar/fullcalendar.js', 'fullcalendar/gcal.js']
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
